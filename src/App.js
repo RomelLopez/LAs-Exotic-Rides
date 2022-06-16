66703f3039f0bbf4c043e08446861cb698956110
RomelLopez <romelanthonylopez@gmail.com>
2022-06-16 14:17:49 -0700

    updated router version

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import Lamborghini from './components/CarPages/Lamborghini';
 import McLaren from './components/CarPages/McLaren';
 import Porsche from './components/CarPages/Porsche';
 import RollsRoyce from './components/CarPages/RollsRoyce';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
     return (
         <div >
             <BrowserRouter>
                 <Routes>
-                    <Route path={process.env.PUBLIC_URL} exact component={Main}></Route>
-                    <Route path="/Ferrari" exact component={Ferrari}></Route>
-                    <Route path="/BMW" exact component={BMW}></Route>
-                    <Route path="/Lamborghini" exact component={Lamborghini}></Route>
-                    <Route path="/McLaren" exact component={McLaren}></Route>
-                    <Route path="/Porsche" exact component={Porsche}></Route>
+                    <Route path={process.env.PUBLIC_URL} element={<Main />}></Route>
+                    <Route path="/Ferrari" element={<Ferrari />}></Route>
+                    <Route path="/BMW" element={<BMW />}></Route>
+                    <Route path="/Lamborghini" element={<Lamborghini />}></Route>
+                    <Route path="/McLaren" element={<McLaren />}></Route>
+                    <Route path="/Porsche" element={<Porsche />}></Route>
                 </Routes>
             </BrowserRouter>
 
